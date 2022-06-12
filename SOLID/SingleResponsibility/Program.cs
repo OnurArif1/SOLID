debce297c6f3204d9163a318f73bc5ea062a5d13
Onur Arif Ciftci <onurarifciftci@gmail.com>
2022-06-12 22:42:07 +0300

    güncelleme

--- SOLID/SingleResponsibility/Program.cs
+++ SOLID/SingleResponsibility/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace SingleResponsibility
 {
     class Program
-    {
+    {//Bir sınıfın sadece bir tane sorumluluğu olması gerekir.Class veya metodun değişmek için bir sebebi olmalı.
         static void Main(string[] args)
         {
             var employe = new Employee();
