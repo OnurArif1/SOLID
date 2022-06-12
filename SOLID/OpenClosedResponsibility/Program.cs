0327c19b2a153c1d35c9ec9c3ed2a09e0e6fded5
Onur Arif Ciftci <onurarifciftci@gmail.com>
2022-06-12 17:47:57 +0300

    testCommit

--- SOLID/OpenClosedResponsibility/Program.cs
+++ SOLID/OpenClosedResponsibility/Program.cs
@@ -6,7 +6,7 @@ namespace OpenClosedResponsibility
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Burak World!");
         }
     }
 }
